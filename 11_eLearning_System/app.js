7b1b84bcddff8f8e92b8d8abb179f6b568cb7cc9
marley-nodejs <marley.nodejs@gmail.com>
2015-10-12 18:50:46 +0300

    11 eLearning System | 002 App  Kickstart Setup Part A

--- 11_eLearning_System/app.js
+++ 11_eLearning_System/app.js
@@ -4,6 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
